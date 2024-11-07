b3b51fea6763110bd0595f9c85b4497ef2caaebb
Akash9611 <gpkp176172@gmail.com>
2024-11-07 16:35:54 +0530

    Field Resolver. Change the column data format while quering the data

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,6 +2,14 @@ import { getJobs } from './db/jobs.js'
 
 export const resolvers = {
     Query: {
-        job: () => getJobs()
+        jobs: () => getJobs()
+    },
+
+    Job: {
+        date: (job) => dateFormatter(job.createdAt)
     }
+};
+
+function dateFormatter(value) {
+    return value.slice(0, 'yyyy-mm-dd'.length);
 }
