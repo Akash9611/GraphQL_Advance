3c12833e0d9bcd7bab7b88a8b219117b3fb8d466
Akash9611 <gpkp176172@gmail.com>
2024-11-07 20:24:10 +0530

    Get JobByID graphql api and context is created and fetching data on frontend on JobPage component

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -2,6 +2,29 @@ import { GraphQLClient, gql } from 'graphql-request'
 
 const client = new GraphQLClient('http://localhost:9000/graphql');
 
+export async function getJob(id) {
+    const query = gql`
+    query JobById($id: ID!) {
+        job(id: $id) {
+            id
+            title
+            date
+            description
+            company {
+              id
+              name
+            }
+        }
+    }
+    `;
+
+    // const data = await client.request(query, { id })
+    // return data.job;
+    //OR directly extract the job object from the data
+    const { job } = await client.request(query, { id })
+    return job;
+}
+
 export async function getJobs() {
     const query = gql`
     query{
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,8 +1,10 @@
 import { getCompany } from './db/companies.js';
-import { getJobs } from './db/jobs.js'
+import { getJob, getJobs } from './db/jobs.js'
 
 export const resolvers = {
     Query: {
+        // job: (_root, args) => {console.log(args) },
+        job: (_root, { id }) => getJob(id),
         jobs: () => getJobs()
     },
 
