dac04fd993f3c480304c1c2b1eda471df251ecec
Akash9611 <gpkp176172@gmail.com>
2024-11-09 13:11:45 +0530

    Error Handling on Backend and frontend side

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -5,16 +5,35 @@ import JobList from '../components/JobList';
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const [company, setCompany] = useState();
+  // const [company, setCompany] = useState();
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false
+  });
 
   useEffect(() => {
-    getCompany(companyId).then(setCompany);
+    // getCompany(companyId).then(setCompany);
+    (async () => {
+      try {
+        const company = await getCompany(companyId);
+        setState({ company, loading: false, error: false });
+      } catch {
+        setState({ company, loading: false, error: true });
+      }
+    })();
+
   }, [companyId])
 
-  console.log('[CompanyPage] Company: ', company);
+  const { company, loading, error } = state;
+  console.log('[CompanyPage] Company: ', state);
 
-  if (!company) {
+  if (loading) {
     return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div className=''>Not Found</div>
   }
   return (
     <div>
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,30 @@
+import { GraphQLError } from 'graphql'
 import { getCompany } from './db/companies.js';
 import { getJob, getJobs, getJobsByCompany } from './db/jobs.js'
 
 export const resolvers = {
     Query: {
-        company: (_root, { id }) => getCompany(id),
+        company: async (_root, { id }) => {
+            const company = await getCompany(id);
+            // if (!company) {
+            //     throw new GraphQLError('Company Not Found with id ' + id, {
+            //         extensions: { code: 'Not Found' }
+            //     })
+            // }
+            //OR create utility function and use it as follows
+            if (!company) {
+                throw NotFoundError('Company Not Found with id ' + id)
+            }
+            return company;
+        },
         // job: (_root, args) => {console.log(args) },
-        job: (_root, { id }) => getJob(id),
+        job: async (_root, { id }) => {
+            const job = await getJob(id);
+            if (!job) {
+                throw NotFoundError('Job not found with id ' + id);
+            }
+            return job;
+        },
         jobs: () => getJobs()
     },
 
@@ -18,6 +37,10 @@ export const resolvers = {
         date: (job) => dateFormatter(job.createdAt)  // Field Conversion. To show date in different format
     }
 };
+
+function NotFoundError(message) {
+    throw new GraphQLError(message, { extensions: { code: 'Not_Found', status: 401 } });
+}
 
 function dateFormatter(value) {
     return value.slice(0, 'yyyy-mm-dd'.length);
