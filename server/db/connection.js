7d9dca062506a940e17f50cf355ea710ca2dc533
Akash9611 <gpkp176172@gmail.com>
2024-11-15 16:34:16 +0530

    Finding N+1 Problem(running multiple queries of fetching same records, leading to inefficiency) To resolve this issue will use DataLoader

--- server/db/connection.js
+++ server/db/connection.js
@@ -7,3 +7,14 @@ export const connection = knex({
   },
   useNullAsDefault: true,
 });
+
+// connection.on('query', (data) => { //* the query object defined here is sql query object not graphql object. The data represent the queries run by the apis on database
+
+//   console.log('[db]: ' , data); //* data basically contains main key context values named sql and binding. Where sql- are the queries run on database and binding - contains the ids or find by column information
+// })
+
+//! In following code we are only extracting necessary context values from `data`- that are {sql, binding}
+connection.on('query', ({ sql, binding }) => {
+  const query = connection.raw(sql, binding).toQuery(); //toQuery is for converting it to string
+  console.log('[db]: ', query)
+}) // This logic we are implementing for looking at how many time the sql queries are running and how to resolve N+1 problem by using the dataLoader package.
